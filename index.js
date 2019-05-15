36a78d784cb9072c0133db4e570e464d7c6f1bdb
Joel Corey <joelcorey@fastmail.com>
2019-05-15 12:14:13 -0700

    mergeSortedArray

--- index.js
+++ index.js
@@ -103,4 +103,22 @@ function maxSum(arr) {
 }
 //console.log(maxSum([4, 6, -3, 5, -2, 1]));
 
-
+function mergeSortedArray(a, b) {
+  var tempArray = [];
+  while(a.length || b.length) {
+    if(typeof a[0] === 'undefined') {
+      tempArray.push(b[0]);
+      b.splice(0,1);
+    } 
+    else if(a[0] > b[0]) {
+      tempArray.push(b[0]);
+      b.splice(0,1);
+    } 
+    else {
+      tempArray.push(a[0]);
+      a.splice(0,1);
+    }
+  }
+  return tempArray;
+}
+console.log(mergeSortedArray([4,6,7,8], [1,2,3,5,9]));
